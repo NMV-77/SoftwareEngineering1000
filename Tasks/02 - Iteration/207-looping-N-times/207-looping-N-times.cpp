20b151a4040ee5c393341045845d92e730942dae
Noel Mathew <noel.varghese@students.plymouth.ac.uk>
2023-11-08 18:37:38 +0000

    did some more studying

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -3,28 +3,47 @@
 int main()
 {
     //Counter variable
-    int i;
+    //int i;
 
 
     //For loop (pre-C99 standard)
-    for (i = 0; i < 5; i++)
+    for (int i = 0; i < 5; i++)
     {
         //Perform code tasks
         printf("i=%d\n", i);
     }
-
+    printf("\n");
     // TASK A - count from 10 down to 0 in steps of -1
-
-    // TASK B - count from 10 down to -10 in steps of -2
-
-    // TASK C - count from 10 to 200 in steps of 10
-
-    // TASK D - count from -10 down to 20 in steps of 5
-
-    // TASK E - can you predict the largest value of i that is displayed?
-    for (i = 0; i < 12; i += 3)
+    for (int i = 10; i >=0; i--)
     {
-        //Perform code tasks
+        printf("i=%d\n", i);
+    }
+    printf("\n");
+    // TASK B - count from 10 down to -10 in steps of -2
+    for (int i = 10; i >= -10; i=i-2)
+    {
+        
+        printf("i=%d\n", i);
+    }
+    printf("\n");
+    // TASK C - count from 10 to 200 in steps of 10
+    for (int i = 10; i <= 200; i=i+10)
+    {
+        
+        printf("i=%d\n", i);
+    }
+    printf("\n");
+    // TASK D - count from -10 down to 20 in steps of 5
+    for (int i = -10; i <= 20; i=i+5)
+    {
+        
+        printf("i=%d\n", i);
+    }
+    printf("\n");
+    // TASK E - can you predict the largest value of i that is displayed?
+    for (int i = 0; i < 12; i += 3)
+    {
+        
         printf("i=%d\n", i);
     }
 }
